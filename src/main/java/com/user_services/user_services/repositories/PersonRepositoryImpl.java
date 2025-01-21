07a00f781fb1d1a8de5bb220cb9cb4320b29e9b1
pepenacho <herreragrej@proton.me>
2025-01-21 20:07:12 -0300

    PersonRepositoryImpl complete

---
+++ src/main/java/com/user_services/user_services/repositories/PersonRepositoryImpl.java
@@ -0,0 +1,20 @@
+package com.user_services.user_services.repositories;
+
+import com.user_services.user_services.model.Person;
+import com.user_services.user_services.repositories.interfaces.PersonRepository;
+import io.vavr.control.Try;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class PersonRepositoryImpl implements PersonRepository {
+  private final Logger logger = LoggerFactory.getLogger(PersonRepositoryImpl.class);
+
+  @Override
+  public Try<Long> create(Person person) {
+    String sql = "INSERT INTO persons ()";
+
+    return Try.of(() -> 2l);
+  }
+}
