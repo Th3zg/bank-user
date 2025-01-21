3df0b4a5cba105743ff551ae535292a38089dddd
pepenacho <herreragrej@proton.me>
2025-01-21 20:22:46 -0300

    UserServices complete

---
+++ src/main/java/com/user_services/user_services/services/interfaces/UserServices.java
@@ -0,0 +1,13 @@
+package com.bank.customer.customer_service.services.interfaces;
+
+import com.user_services.user_services.dto.request.CreateUserRequest;
+import com.user_services.user_services.util.Result;
+
+public interface UserServices {
+  Result<?> getAllCustomers();
+  Result<?> getCustomerById();
+  Result<?> createCustomer(CreateUserRequest request);
+  Result<?> updateCustomer();
+  Result<?> deleteCustomer();
+  Result<?> searchCustomers();
+}
