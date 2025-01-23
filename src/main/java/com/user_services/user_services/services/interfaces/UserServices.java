63d2374c72ea46f61e99887f9c0cfea5f248f59f
pepenacho <herreragrej@proton.me>
2025-01-22 21:00:49 -0300

    UserServices complete

--- src/main/java/com/user_services/user_services/services/interfaces/UserServices.java
+++ src/main/java/com/user_services/user_services/services/interfaces/UserServices.java
@@ -4,10 +4,10 @@ import com.user_services.user_services.dto.request.CreateUserRequest;
 import com.user_services.user_services.util.Result;
 
 public interface UserServices {
-  Result<?> getAllCustomers();
-  Result<?> getCustomerById();
-  Result<?> createCustomer(CreateUserRequest request);
-  Result<?> updateCustomer();
-  Result<?> deleteCustomer();
-  Result<?> searchCustomers();
+  Result<?> getAllUsers();
+  Result<?> getUserById();
+  Result<?> createUser(CreateUserRequest request);
+  Result<?> updateUser();
+  Result<?> deleteUser();
+  Result<?> searchUsers();
 }
