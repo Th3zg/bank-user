ddef3ca2995a0b94d06c3d65a050714f249e0d88
pepenacho <herreragrej@proton.me>
2025-02-05 15:59:01 -0300

    changes in Result

--- src/main/java/com/user_services/user_services/util/Result.java
+++ src/main/java/com/user_services/user_services/util/Result.java
@@ -1,22 +1,8 @@
 package com.user_services.user_services.util;
 
-import lombok.Getter;
-
 import java.util.Set;
 
-public class Result<T> {
-  @Getter
-  private final T value;
-  private final boolean isSuccess;
-  @Getter
-  private final Set<String> errors;
-
-  private Result(T value, boolean isSuccess, Set<String> errors ) {
-    this.value = value;
-    this.isSuccess = isSuccess;
-    this.errors = errors;
-  }
-
+public record Result<T>(T value, boolean isSuccess, Set<String> errors) {
   public static <T> Result<T> success(T value) {
     return new Result<>(value, true, null);
   }
@@ -29,20 +15,7 @@ public class Result<T> {
     return new Result<>(null, false, errors);
   }
 
-  public boolean isSuccess() {
-    return isSuccess;
-  }
-
   public boolean isFailure() {
     return !isSuccess;
-  }
-
-  @Override
-  public String toString() {
-    return "Result{" +
-            "value=" + value +
-            ", isSuccess=" + isSuccess +
-            ", errors=" + errors +
-            '}';
   }
 }
