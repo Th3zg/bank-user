3f84f39b3045e9cdb7fdb841c3c10104782ffa8b
pepenacho <herreragrej@proton.me>
2025-02-06 15:39:38 -0300

    add changes

--- src/main/java/com/user_services/user_services/repositories/interfaces/PersonRepository.java
+++ src/main/java/com/user_services/user_services/repositories/interfaces/PersonRepository.java
@@ -1,6 +1,6 @@
 package com.user_services.user_services.repositories.interfaces;
 
-import com.user_services.user_services.domain.Person;
+import com.user_services.user_services.model.Person;
 import io.vavr.control.Try;
 
 public interface PersonRepository {
