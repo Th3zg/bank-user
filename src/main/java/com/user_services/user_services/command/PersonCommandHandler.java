07bc9fa99b10e838ef477fa00fadd4097396987b
pepenacho <herreragrej@proton.me>
2025-02-07 19:55:25 -0300

    add changes

--- src/main/java/com/user_services/user_services/command/PersonCommandHandler.java
+++ src/main/java/com/user_services/user_services/command/PersonCommandHandler.java
@@ -1,10 +1,12 @@
 package com.user_services.user_services.command;
 
+import com.user_services.user_services.events.PersonCreatedEvent;
 import com.user_services.user_services.model.Person;
 import com.user_services.user_services.repositories.PersonRepositoryImpl;
 import com.user_services.user_services.util.Result;
 import io.vavr.control.Try;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.support.TransactionTemplate;
 
@@ -15,19 +17,12 @@ import java.util.Collections;
 public class PersonCommandHandler {
   private final PersonRepositoryImpl personRepository;
   private final TransactionTemplate transactionTemplate;
+  private final ApplicationEventPublisher eventPublisher;
   
   public Result<Void> handle(CreatePersonCommand command) {
     return transactionTemplate.execute(status -> {
-      Person person = new Person.Builder()
-              .setFirstName(command.firstName())
-              .setLastName((command.lastName()))
-              .setDateBirth(command.dateBirth())
-              .setEmail(command.email())
-              .setPassword(command.password())
-              .setGender(command.gender())
-              .setProfileImageUrl(command.profileImageUrl())
-              .setCommunicationPreference(command.communicationPreference())
-              .build();
+      // create the person
+      Person person = createPerson(command);
 
       Try<Long> resultPersonCreation = personRepository.create(person);
       if (resultPersonCreation.isFailure()) {
@@ -35,8 +30,29 @@ public class PersonCommandHandler {
         return Result.failure(Collections.singleton("Error: " + resultPersonCreation.getCause().getMessage()));
       }
 
+      //get id of the peron
       Long id = resultPersonCreation.get();
+
+      eventPublisher.publishEvent(new PersonCreatedEvent(id,
+              person.getFirstName(),
+              person.getLastName(),
+              person.getEmail(),
+              person.getDateBirth()));
+
       return Result.success();
     });
+  }
+
+  private Person createPerson(CreatePersonCommand command) {
+    return new Person.Builder()
+            .setFirstName(command.firstName())
+            .setLastName((command.lastName()))
+            .setDateBirth(command.dateBirth())
+            .setEmail(command.email())
+            .setPassword(command.password())
+            .setGender(command.gender())
+            .setProfileImageUrl(command.profileImageUrl())
+            .setCommunicationPreference(command.communicationPreference())
+            .build();
   }
 }
