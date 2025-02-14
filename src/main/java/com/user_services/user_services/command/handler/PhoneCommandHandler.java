d93521b310726e02fbb85daa18e502f060f102fd
pepenacho <herreragrej@proton.me>
2025-02-14 15:45:49 -0300

    add changes

---
+++ src/main/java/com/user_services/user_services/command/handler/PhoneCommandHandler.java
@@ -0,0 +1,45 @@
+package com.user_services.user_services.command.handler;
+
+import com.user_services.user_services.command.CreatePhoneCommand;
+import com.user_services.user_services.model.entity.Phone;
+import com.user_services.user_services.repositories.PhoneRepositoryImpl;
+import com.user_services.user_services.util.Result;
+import io.vavr.control.Try;
+import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.support.TransactionTemplate;
+
+import java.util.Collections;
+
+@Component
+@RequiredArgsConstructor
+public class PhoneCommandHandler {
+  private final Logger logger = LoggerFactory.getLogger(PhoneCommandHandler.class);
+  private final PhoneRepositoryImpl phoneRepository
+  private final TransactionTemplate transactionTemplate;
+
+  public Result<Void> handler(Long personId, CreatePhoneCommand command) {
+    return transactionTemplate.execute(status -> {
+      // create the phone
+      Phone phone = createPhone(personId, command);
+
+      Try<Void> resultPhoneCreation = phoneRepository.create(phone);
+      if (resultPhoneCreation.isFailure()) {
+        status.setRollbackOnly();
+        return Result.failure(Collections.singleton("Error: " + resultPhoneCreation.getCause().getMessage()));
+      }
+
+      return Result.success();
+    });
+  }
+  private Phone createPhone(long personsId, CreatePhoneCommand command) {
+    return new Phone.Builder()
+            .setPhoneId(personsId)
+            .setPhoneNumber(command.phoneNumber())
+            .setPhoneType(command.phoneType())
+            .build();
+  }
+}
+
