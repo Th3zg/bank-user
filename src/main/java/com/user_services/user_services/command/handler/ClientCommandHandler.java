8ca9a7c2b723e1bb933b5e8925b102fe500b8148
pepenacho <herreragrej@proton.me>
2025-02-14 14:45:16 -0300

    add changes

---
+++ src/main/java/com/user_services/user_services/command/handler/ClientCommandHandler.java
@@ -0,0 +1,55 @@
+package com.user_services.user_services.command.handler;
+
+import com.user_services.user_services.command.CreateClientCommand;
+import com.user_services.user_services.model.entity.Client;
+import com.user_services.user_services.repositories.ClientRepositoryImpl;
+import com.user_services.user_services.util.Result;
+import io.vavr.control.Try;
+import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.support.TransactionTemplate;
+
+import java.util.Collections;
+
+@Component
+@RequiredArgsConstructor
+public class ClientCommandHandler {
+  private final Logger logger = LoggerFactory.getLogger(ClientCommandHandler.class);
+  private final ClientRepositoryImpl clientRepository;
+  private final TransactionTemplate transactionTemplate;
+
+  public Result<Void> handler(long personsId, CreateClientCommand command) {
+    return transactionTemplate.execute(status -> {
+      // create the client
+      Client client = createClient(personsId, command);
+
+      Try<Void> resultClientCreation = clientRepository.create(client);
+      if (resultClientCreation.isFailure()) {
+        status.setRollbackOnly();
+        return Result.failure(Collections.singleton("Error: " + resultClientCreation.getCause().getMessage()));
+      }
+
+      return Result.success();
+    });
+  }
+
+  private Client createClient(long personsId, CreateClientCommand command) {
+    return new Client.Builder()
+            .setPersonId(personsId)
+            .setAccountNumber(command.accountNumber())
+            .setAccountBalance(command.accountBalance())
+            .setOverdraftLimit(command.overdraftLimit())
+            .setRiskLevel(command.riskLevel())
+            .setCreditScore(command.creditScore())
+            .setTotalLoans(command.totalLoans())
+            .setTotalInsurance(command.totalInsurance())
+            .setTotalInvestments(command.totalInvestments())
+            .setMonthlyIncome(command.monthlyIncome())
+            .setOccupation(command.occupation())
+            .setMaritalStatus(command.maritalStatus())
+            .build();
+  }
+}
+
