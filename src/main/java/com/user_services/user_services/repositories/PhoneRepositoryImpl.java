f680210a44640b7fa69c794b30f4348212436e33
pepenacho <herreragrej@proton.me>
2025-02-16 00:54:08 -0300

    add changes

---
+++ src/main/java/com/user_services/user_services/repositories/PhoneRepositoryImpl.java
@@ -0,0 +1,32 @@
+package com.user_services.user_services.repositories;
+
+import com.user_services.user_services.exception.DatabaseErrorExceptionMapper;
+import com.user_services.user_services.model.entity.Phone;
+import io.vavr.control.Try;
+import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.dao.DataAccessException;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+@RequiredArgsConstructor
+public class PhoneRepositoryImpl {
+  private final Logger logger = LoggerFactory.getLogger(PersonRepositoryImpl.class);
+  private final JdbcTemplate jdbcTemplate;
+
+  public Try<Void> create(Phone phone) {
+    String sql = "INSERT INTO phones (person_id, phone_number, phone_type) VALUES (?, ?, ?)";
+
+    return Try.run(() -> {
+      jdbcTemplate.update(sql,
+              phone.getPersonId(),
+              phone.getPhoneNumber(),
+              phone.getPhoneType()
+      );
+      logger.info("Phone created successfully");
+    }).onFailure(ex -> {
+      logger.error("Error creating client", ex);
+      DatabaseErrorExceptionMapper.fromException((DataAccessException) ex);
+    });
+  }
+}
