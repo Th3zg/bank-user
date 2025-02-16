977a8a69d5447b6eb21a48804877c3f904a6a2ad
pepenacho <herreragrej@proton.me>
2025-02-16 16:10:44 -0300

    add changes

--- src/main/java/com/user_services/user_services/events/ClientCreatedEvent.java
+++ src/main/java/com/user_services/user_services/events/ClientCreatedEvent.java
@@ -1,5 +1,6 @@
 package com.user_services.user_services.events;
 
+import com.user_services.user_services.enums.RiskLevel;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
@@ -8,9 +9,33 @@ public record ClientCreatedEvent(
         Long personId,
         String accountNumber,
         BigDecimal accountBalance,
+        BigDecimal overdraftLimit,
+        RiskLevel riskLevel,
+        BigDecimal creditScore,
+        BigDecimal totalLoans,
+        BigDecimal totalInvestments,
+        BigDecimal totalInsurance,
+        BigDecimal monthlyIncome,
+        String occupation,
+        String maritalStatus,
         LocalDateTime createdAt
 ) {
   public ClientCreatedEvent(Long clientId, Long personId, String accountNumber, BigDecimal accountBalance) {
-    this(clientId, personId, accountNumber, accountBalance, LocalDateTime.now());
+    this(
+            clientId,
+            personId,
+            accountNumber,
+            accountBalance,
+            BigDecimal.ZERO,
+            RiskLevel.LOW,
+            BigDecimal.ZERO,
+            BigDecimal.ZERO,
+            BigDecimal.ZERO,
+            BigDecimal.ZERO,
+            BigDecimal.ZERO,
+            "Unknown",
+            "Unknown",
+            LocalDateTime.now()
+    );
   }
 }
