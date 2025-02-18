fa5227b8cec13585b0a9a2b362fbbf43b3c10e7a
pepenacho <herreragrej@proton.me>
2025-02-18 20:29:29 -0300

    add changes

--- src/main/java/com/user_services/user_services/outbox/OutboxEvent.java
+++ src/main/java/com/user_services/user_services/outbox/OutboxEvent.java
@@ -1,12 +1,15 @@
 package com.user_services.user_services.outbox;
 
+import com.user_services.user_services.enums.OutboxStatus;
+
 import java.time.LocalDateTime;
 
 public record OutboxEvent(
         Long id,
         String aggregate_type,
         long aggregate_id,
+        String type,
         Object payload,
-        OutboxEvent status,
+        OutboxStatus status,
         LocalDateTime createdAt
 ) {}
