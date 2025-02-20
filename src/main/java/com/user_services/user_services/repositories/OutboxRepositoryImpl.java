be2d1c69896f655d580143747a3f68e90c728f52
pepenacho <herreragrej@proton.me>
2025-02-20 18:44:17 -0300

    add changes

--- src/main/java/com/user_services/user_services/repositories/OutboxRepositoryImpl.java
+++ src/main/java/com/user_services/user_services/repositories/OutboxRepositoryImpl.java
@@ -1,12 +1,16 @@
 package com.user_services.user_services.repositories;
 
+import com.user_services.user_services.enums.OutboxStatus;
 import com.user_services.user_services.outbox.OutboxEvent;
 import com.user_services.user_services.repositories.interfaces.OutboxRepository;
+import com.user_services.user_services.util.Json;
+import io.vavr.control.Try;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.util.List;
 
@@ -15,6 +19,7 @@ import java.util.List;
 public class OutboxRepositoryImpl implements OutboxRepository {
   private final Logger logger = LoggerFactory.getLogger(OutboxRepositoryImpl.class);
   private final JdbcTemplate jdbcTemplate;
+  private final ObjectMapper objectMapper;
 
   @Override
   public List<OutboxEvent> findByProcessedFalse() {
@@ -22,7 +27,54 @@ public class OutboxRepositoryImpl implements OutboxRepository {
   }
 
   @Override
-  public void markAsProcessed(Long id) {
+  public Try<Void> markAsProcessed(Long id) {
+    String sql = """
+            UPDATE outbox_events
+            SET status = 'PROCESSED'
+            WHERE id = ?
+            """;
 
+    return Try.run(() -> {
+      jdbcTemplate.update(sql, id);
+    });
+  }
+
+  @Override
+  public Try<Void> insert(OutboxEvent event) {
+    String sql = """
+            INSERT INTO outbox_events (aggregate_type, aggregate_id, type, payload, status, created_at)
+            VALUES (?, ?, ?, CAST(? AS JSONB), ?, ?)
+        """;
+
+    String payloadJson = Json.convertPayloadToJson(event.payload());
+
+    return Try.run(() -> {
+      jdbcTemplate.update(sql,
+              event.aggregate_type(),
+              event.aggregate_id(),
+              event.type(),
+              event.payload(),
+              event.status());
+    });
+  }
+
+  @Override
+  public Try<List<OutboxEvent>> findPendingEvents() {
+    String sql = """
+            SELECT * FRM outbox_events
+            WHERE status = PENDING
+            ORDER BY created_at ASC
+            """;
+
+    return Try.of(() ->
+            jdbcTemplate.query(sql, (rs, rowNum) -> new OutboxEvent(
+                    rs.getLong("id"),
+                    rs.getString("aggregate_type"),
+                    rs.getLong("aggregate_id"),
+                    rs.getString("type"),
+                    rs.getObject("payload"),
+                    OutboxStatus.valueOf(rs.getString("status"))
+            ))
+    );
   }
 }
