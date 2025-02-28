a7ed2e00a4c549a821f0a567f40288c6e2cd41aa
pepenacho <herreragrej@proton.me>
2025-02-28 11:08:30 -0300

    add changes

--- src/main/java/com/user_services/user_services/services/OutboxProcessor.java
+++ src/main/java/com/user_services/user_services/services/OutboxProcessor.java
@@ -5,6 +5,8 @@ import com.user_services.user_services.repositories.OutboxRepositoryImpl;
 import com.user_services.user_services.util.Result;
 import io.vavr.control.Try;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
@@ -16,6 +18,7 @@ import java.util.List;
 @Service
 @RequiredArgsConstructor
 public class OutboxProcessor {
+  private final Logger logger  = LoggerFactory.getLogger(OutboxProcessor.class);
   private final OutboxRepositoryImpl outboxRepository;
   private final KafkaTemplate<String, Object> kafkaTemplate;
   private final TransactionTemplate transactionTemplate;
@@ -23,13 +26,14 @@ public class OutboxProcessor {
   @Scheduled(fixedDelay = 5000)
   public Result<Void> processOutboxEvents() {
     return transactionTemplate.execute(status -> {
-      Try<List<OutboxEvent>> tryEvents = outboxRepository.findPendingEvents();
+      Try<List<OutboxEvent>> outboxLists = outboxRepository.findPendingEvents();
 
-      if (tryEvents.isFailure()) {
-        return Result.failure(Collections.singleton("Error: " + tryEvents.getCause().getMessage()));
+      if (outboxLists.isFailure()) {
+        status.setRollbackOnly();
+        return Result.failure(Collections.singleton("Error: " + outboxLists.getCause().getMessage()));
       }
 
-      List<OutboxEvent> events =  tryEvents.get();
+      List<OutboxEvent> events = outboxLists.get();
       events.forEach(event -> {
         kafkaTemplate.send(event.type(), event.payload());
         outboxRepository.markAsProcessed(event.id());
