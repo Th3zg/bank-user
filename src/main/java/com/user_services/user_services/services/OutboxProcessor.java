1f959f94bc25127de0f9e58e8f28f58b3318454b
pepenacho <herreragrej@proton.me>
2025-02-28 17:00:01 -0300

    add changes

--- src/main/java/com/user_services/user_services/services/OutboxProcessor.java
+++ src/main/java/com/user_services/user_services/services/OutboxProcessor.java
@@ -10,9 +10,9 @@ import org.slf4j.LoggerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.TransactionStatus;
 import org.springframework.transaction.support.TransactionTemplate;
 
-import java.util.Collections;
 import java.util.List;
 
 @Service
@@ -25,20 +25,40 @@ public class OutboxProcessor {
 
   @Scheduled(fixedDelay = 5000)
   public Result<Void> processOutboxEvents() {
-    return transactionTemplate.execute(status -> {
-      Try<List<OutboxEvent>> outboxLists = outboxRepository.findPendingEvents();
+    return transactionTemplate.execute(status ->
+            outboxRepository.findPendingEvents()
+                    .fold(error -> {
+                      logger.error("Error fetching events", error);
+                      status.setRollbackOnly();
+                      return Result.failure("Fetch error: " + error.getMessage());
+                            },
+                            events -> processEvents(events, status)));
+  }
 
-      if (outboxLists.isFailure()) {
-        status.setRollbackOnly();
-        return Result.failure(Collections.singleton("Error: " + outboxLists.getCause().getMessage()));
-      }
+  private Result<Void> processEvents(List<OutboxEvent> events, TransactionStatus status) {
+    List<Try<Void>> processingResults = events.stream()
+            .map(this::processSingleEvent)
+            .toList();
 
-      List<OutboxEvent> events = outboxLists.get();
-      events.forEach(event -> {
-        kafkaTemplate.send(event.type(), event.payload());
-        outboxRepository.markAsProcessed(event.id());
-      });
-      return Result.success();
-    });
+    List<String> errors = processingResults.stream()
+            .filter(Try::isFailure)
+            .map(e -> e.getCause().getMessage())
+            .toList();
+
+    if (errors.isEmpty()) {
+      status.setRollbackOnly();
+      return Result.failure(errors);
+    }
+
+    return Result.success();
+  }
+
+  private Try<Void> processSingleEvent(OutboxEvent event) {
+    return Try.run(() ->
+            kafkaTemplate.send(event.type(), event.payload()).get()
+    )
+    .flatMap((v) -> outboxRepository.markAsProcessed(event.id()))
+    .onFailure(ex ->
+            logger.error("Failed processing event {}", event.id(), ex));
   }
 }
