2965a61eb3e592142c5145a1fd17070d236880bb
pepenacho <herreragrej@proton.me>
2025-02-28 16:40:05 -0300

    add changes

--- src/main/java/com/user_services/user_services/util/Result.java
+++ src/main/java/com/user_services/user_services/util/Result.java
@@ -1,8 +1,25 @@
 package com.user_services.user_services.util;
 
-import java.util.Set;
+import java.util.Collections;
+import java.util.List;
 
-public record Result<T>(T value, boolean isSuccess, Set<String> errors) {
+public record Result<T>(T value, boolean isSuccess, List<String> errors) {
+
+  public Result {
+    errors = Collections.unmodifiableList(errors);
+    validateState(isSuccess, errors);
+  }
+
+  private Result<Void> validateState(boolean isSuccess, List<String> errors) {
+    if (isSuccess && !errors.isEmpty()) {
+      return Result.failure("Successful results cannot have errors");
+    }
+
+    if (!isSuccess && errors.isEmpty()) {
+      return Result.failure("Failed results require at least 1 error");
+    }
+  }
+
   public static <T> Result<T> success(T value) {
     return new Result<>(value, true, null);
   }
@@ -11,11 +28,16 @@ public record Result<T>(T value, boolean isSuccess, Set<String> errors) {
     return new Result<>(null, true, null);
   }
 
-  public static <T>  Result<T> failure(Set<String> errors) {
+  public static <T>  Result<T> failure(List<String> errors) {
     return new Result<>(null, false, errors);
   }
+
+  public static <T> Result<T> failure(String error) { return new Result<>(null, false, Collections.singletonList(error)); }
+
+  public List<String> getUniqueError() { return errors.stream().distinct().toList(); }
 
   public boolean isFailure() {
     return !isSuccess;
   }
+  public boolean isSuccess() { return isSuccess; }
 }
