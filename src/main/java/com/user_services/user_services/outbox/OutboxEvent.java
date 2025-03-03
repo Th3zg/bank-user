3dc5ba369fdc9cbc70e3424bb9dc92289df9a395
pepenacho <herreragrej@proton.me>
2025-03-03 17:13:18 -0300

    add changes

--- src/main/java/com/user_services/user_services/outbox/OutboxEvent.java
+++ src/main/java/com/user_services/user_services/outbox/OutboxEvent.java
@@ -1,12 +1,14 @@
 package com.user_services.user_services.outbox;
 
+import com.user_services.user_services.enums.AggregateType;
+import com.user_services.user_services.enums.EventType;
 import com.user_services.user_services.enums.OutboxStatus;
 
 public record OutboxEvent(
         Long id,
-        String aggregate_type,
+        AggregateType aggregate_type,
         long aggregate_id,
-        String type,
+        EventType type,
         Object payload,
         OutboxStatus status
 ) {}
