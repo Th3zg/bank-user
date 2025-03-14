16ff5fa7eddebae0554cf64e5d333e9363339119
pepenacho <herreragrej@proton.me>
2025-03-14 00:38:41 -0300

    add changes

--- src/main/java/com/user_services/user_services/dto/error/ErrorResponse.java
+++ src/main/java/com/user_services/user_services/dto/error/ErrorResponse.java
@@ -1,52 +1,26 @@
 package com.user_services.user_services.dto.error;
 
+import com.user_services.user_services.enums.ErrorCode;
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 
 import java.time.LocalDateTime;
 import java.util.Set;
 
-@Getter
-public class ErrorResponse {
-  private final String errorCode;
-  private final Set<String> messages;
-  private final String message;
-  private final ErrorDetails details;
-  private final LocalDateTime timestamp;
+public record ErrorResponse(
+        ErrorCode errorCode,
+        Set<String> messages, 
+        String message,
+        ErrorDetails details,
+        LocalDateTime timestamp,
+        int statusCode
+) {
 
-  public ErrorResponse(String errorCode, Set<String> messages, String message, ErrorDetails details) {
-    this.errorCode = errorCode;
-    this.messages = messages;
-    this.message = message;
-    this.details = details;
-    this.timestamp = LocalDateTime.now();
+  public ErrorResponse( ErrorCode errorCode, Set<String> messages, String message, ErrorDetails details, int statusCode) {
+    this(errorCode, messages, message, details, LocalDateTime.now(), statusCode);
   }
-
-  @Override
-  public String toString() {
-    return "ErrorResponse{" +
-            "errorCode='" + errorCode + '\'' +
-            ", messages='" + messages + '\'' +
-            ", message='" + message + '\'' +
-            ", details=" + details +
-            ", timestamp=" + timestamp +
-            '}';
-  }
-
-  public static class ErrorDetails {
-    private final String providedValue;
-    private final Set<String> acceptedValues;
-
-    public ErrorDetails(String providedValue, Set<String> acceptedValues) {
-      this.providedValue = providedValue;
-      this.acceptedValues = acceptedValues;
-    }
-
-    @Override
-    public String toString() {
-      return "ErrorDetails{" +
-              "providedValue='" + providedValue + '\'' +
-              ", acceptedValues=" + acceptedValues +
-              '}';
-    }
-  }
+  public record ErrorDetails(
+          String providedValue,
+          Set<String> acceptedValues
+  ) {}
 }
