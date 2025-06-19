96a40b73906beed3ef7c26d39002875d31138fde
pepenacho <herreragrej@proton.me>
2025-06-19 14:37:27 -0400

    add changes

---
+++ src/main/java/com/user_services/user_services/kafka/services/KafkaMessageService.java
@@ -0,0 +1,32 @@
+package com.user_services.user_services.kafka.services;
+
+import com.user_services.user_services.kafka.interfaces.KafkaSendCallback;
+import com.user_services.user_services.outbox.OutboxEvent;
+import com.user_services.user_services.repositories.OutboxRepositoryImpl;
+import io.github.resilience4j.circuitbreaker.CircuitBreaker;
+import io.github.resilience4j.retry.Retry;
+import lombok.RequiredArgsConstructor;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.support.SendResult;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class KafkaMessageService implements KafkaSendCallback {
+  private final Logger logger = LoggerFactory.getLogger(KafkaMessageService.class);
+
+  private final OutboxRepositoryImpl outboxRepository;
+  private final Retry kafkaRetry;
+  private final CircuitBreaker kafkaCircuitBreaker;
+
+  public void onSuccess(OutboxEvent event, SendResult<String, Object> result) { // logeo, metricas,etc
+    logger.info("Successfully sent event {} to Kafka.", event.id());
+    result.getRecordMetadata();
+  }
+
+  public void onFailure(OutboxEvent event, ProducerRecord<String, OutboxEvent> record, Throwable ex) {
+    logger.error("Failed to send event {} to Kafka topic {}. Exception: {}", event.id(), record.topic(), ex.getMessage(), ex);
+  }
+}
