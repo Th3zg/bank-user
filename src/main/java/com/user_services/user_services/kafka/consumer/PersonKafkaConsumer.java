b2b4338a8bae8e68be5a63d85ac74c2e8f815ea6
pepenacho <herreragrej@proton.me>
2025-06-21 13:36:33 -0400

    add changes

--- src/main/java/com/user_services/user_services/kafka/consumer/PersonKafkaConsumer.java
+++ src/main/java/com/user_services/user_services/kafka/consumer/PersonKafkaConsumer.java
@@ -3,10 +3,13 @@ package com.user_services.user_services.kafka.consumer;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.user_services.user_services.events.PersonCreatedEvent;
 import com.user_services.user_services.services.PersonProjectionService;
+import io.github.resilience4j.circuitbreaker.CircuitBreaker;
+import io.github.resilience4j.retry.Retry;
 import io.vavr.control.Try;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.kafka.annotation.DltHandler;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
@@ -16,20 +19,26 @@ public class PersonKafkaConsumer {
   private final Logger logger = LoggerFactory.getLogger(PersonKafkaConsumer.class);
   private final ObjectMapper objectMapper;
   private final PersonProjectionService personProjectionService;
+  private final Retry personProjectionRetry;
+  private final CircuitBreaker personProjectionBreaker;
+
 
   // @RetryableTopic(attempts = "3", backoff = @Backoff(delay = 1000))
   @KafkaListener(topics = "PersonCreatedEvent", groupId = "couchbase-sync-group")
   public void handlePersonCreatedEvent(String eventJsonb) {
     Try.of(() -> objectMapper.readValue(eventJsonb, PersonCreatedEvent.class))
             .onSuccess(event -> {
-              personProjectionService.CreatePersonDocument(event);
+              Runnable projectionTask = () -> personProjectionService.CreatePersonDocument(event);
+              Runnable resilientTask = CircuitBreaker.decorateRunnable(personProjectionBreaker, Retry.decorateRunnable(personProjectionRetry, projectionTask));
+              resilientTask.run();
               logger.info("Person document synchronized in Couchbase: {}", event.personId());
               // ack.acknowledge(); // Confirmar offset manualmente
             }).onFailure(e -> logger.error("Error deserializing PersonCreatedEvent: {}", e.getMessage(), e));
   }
 
-//  @DltHandler
-//  public void handlePersonCreatedEvent(String eventJsonb) {
-//    logger.error("Evento no procesable enviado al DLT: {}", eventJsonb);
-//  }
+  @DltHandler
+  public void handleFailedEvent(String eventJson) {
+    logger.error("Mensaje enviado a DLT: {}", eventJson);
+  }
+
 }
