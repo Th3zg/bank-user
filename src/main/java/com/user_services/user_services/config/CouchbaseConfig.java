f0db9ec9846f4d00bca094cce0c1b698adcca90a
pepenacho <herreragrej@proton.me>
2025-06-25 15:29:44 -0400

    add changes

--- src/main/java/com/user_services/user_services/config/CouchbaseConfig.java
+++ src/main/java/com/user_services/user_services/config/CouchbaseConfig.java
@@ -4,35 +4,21 @@ import com.couchbase.client.java.Cluster;
 import com.couchbase.client.java.Bucket;
 import com.couchbase.client.java.Collection;
 import jakarta.annotation.PreDestroy;
-import org.springframework.beans.factory.annotation.Value;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import java.time.Duration;
-
 @Configuration
+@RequiredArgsConstructor
+@EnableConfigurationProperties(CouchbaseProperties.class)
 public class CouchbaseConfig {
-
+  private final CouchbaseProperties couchbaseProperties;
   private Cluster cluster;
-  @Value("${couchbase.connection-string}")
-  private String connectionString;
-  @Value("${couchbase.username}")
-  private String username;
-  @Value("${couchbase.password}")
-  private String password;
-  @Value("${couchbase.bucket.name}")
-  private String bucketName;
-
-  @Bean
-  public Cluster couchbaseCluster() {
-    return Cluster.connect(connectionString, username, password);
-  }
 
   @Bean
   public Bucket couchbaseBucket(Cluster cluster) {
-    Bucket bucket = cluster.bucket(bucketName);
-    bucket.waitUntilReady(Duration.ofSeconds(10));
-    return bucket;
+    return cluster.bucket(couchbaseProperties.getBucketName());
   }
 
   @Bean
